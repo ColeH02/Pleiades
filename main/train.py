3d457b07c8a4ec94c88a4ca8bc8c0d91995505ff
Charlie Taylor <cet008@bucknell.edu>
2021-09-11 01:45:27 -0400

    setting up training

---
+++ main/train.py
@@ -0,0 +1,27 @@
+import os
+import tangram
+
+def main():
+
+	# Get the path to the .tangram file.
+	model_path = os.path.join(os.path.dirname(__file__), 'hackdata.tangram')
+	# Load the model from the path.
+	model = tangram.Model.from_path(model_path)
+
+	# Create an example input matching the schema of the CSV file the model was trained on.
+	# Here the data is just hard-coded, but in your application you will probably get this
+	# from a database or user input.
+	input = {
+		'sex': 'female',
+		'zodiac': 'aquarius',
+		'sexsex5': 'exclusively female'
+	}
+
+	# Make the prediction!
+	output = model.predict(input)
+
+	# Print the output.
+	print('Output:', output)
+
+if __name__ == "__main__":
+	main()
