86c87006410338b94518f3e2b61e4bf0a42cef33
Charlie Taylor <cet008@bucknell.edu>
2021-09-11 12:40:06 -0400

    ALQ

--- main/train.py
+++ main/train.py
@@ -11,7 +11,7 @@ from main.PleiadesTracker import get_pleiades_pos, final_val, point_arr, point_t
 def main():
 
 	# Get the path to the .tangram file.
-	model_path = os.path.join(os.path.dirname(__file__), 'hackdata2.tangram')
+	model_path = os.path.join(os.path.dirname(__file__), 'hackdata3.tangram')
 	# Load the model from the path.
 	model = tangram.Model.from_path(model_path)
 
@@ -20,8 +20,11 @@ def main():
 	# from a database or user input.
 	input = {
 		'sex': 'female',
+		'degree': 'high school',
 		'zodiac': 'cancer',
-		'preference': 'exclusively female'
+		'sexorient': 'exclusively male',
+		'sociability': 'neither agree nor disagree',
+		'acqmark': '2-5'
 	}
 
 	# Make the prediction!
@@ -30,18 +33,31 @@ def main():
 	# Print the output.
 	print('Full Output:', output)
 	classification = getattr(output, 'class_name')
-	probability = getattr(output, 'probability')
+	confidence = getattr(output, 'probability')
 	print('classification:', classification)
-	print('probability:', probability)
+
+	if(classification == 'not at all'):
+		class_val = 0.1
+	elif (classification == '2-3 times a month'):
+		class_val = 0.25
+	elif (classification == 'weekly'):
+		class_val = 0.5
+	elif (classification == '2-3 per week'):
+		class_val = 0.75
+
+	print('confidence:', confidence)
+	print('class_val:', class_val)
 
 	point = get_pleiades_pos(earth, ra_hours, dec_degrees)
 	print(point)
 	pleiades_location = final_val(point_arr(point_to_str(point)))
-	print(pleiades_location)
+	print('pleiades_location:', pleiades_location)
 
 	universe_freq = 0.432;
+	zodiac_symbols = 12;
+	pleiades_star_count = 800;
 
-	ALQ = probability * pleiades_location / universe_freq * 12
+	ALQ = (abs(math.sin(((confidence * class_val * universe_freq) / (pleiades_location / zodiac_symbols)) * pleiades_star_count))) * 100;
 	print('ALQ:', ALQ)
 
 
