292481e6072a1b4eeab5d0d986d95bbdb2811d20
Charlie Taylor <cet008@bucknell.edu>
2021-09-11 22:40:30 -0400

    fixed everything

--- main/app.py
+++ main/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, Response, request
+from flask import Flask, render_template, Response, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -6,6 +6,20 @@ app = Flask(__name__)
 @app.route('/')
 def local_website():
     return render_template('index.html')
+
+
+@app.route('/success/<name>')
+def success(name):
+    return f'welcome {name}'
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        user = request.form
+        return redirect(url_for('success', name=user))
+    else:
+        return render_template('login.html')
 
 
 if __name__ == '__main__':
